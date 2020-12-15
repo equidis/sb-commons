463712def0d504fbda3dfd90ef1761cb6df43cc8
jntakpe <jntakpe@gmail.com>
2020-12-15 22:28:40 +0000

    feat: add commons-management module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "sb-commons"
 include("sb-commons-cache")
 include("sb-commons-cache-test")
 include("sb-commons-context")
+include("sb-commons-management")
 include("sb-commons-mongo")
 include("sb-commons-mongo-test")
 include("sb-commons-test")
