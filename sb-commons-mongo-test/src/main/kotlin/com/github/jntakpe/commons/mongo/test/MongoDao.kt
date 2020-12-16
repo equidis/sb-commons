bd25fce5d6fc65cdf25b1471c27de92323be1a03
jntakpe <jntakpe@gmail.com>
2020-12-16 22:43:01 +0000

    feat: MongoDao implements TestInitializer

--- sb-commons-mongo-test/src/main/kotlin/com/github/jntakpe/commons/mongo/test/MongoDao.kt
+++ sb-commons-mongo-test/src/main/kotlin/com/github/jntakpe/commons/mongo/test/MongoDao.kt
@@ -3,16 +3,17 @@ package com.github.jntakpe.commons.mongo.test
 import com.github.jntakpe.commons.mongo.Identifiable
 import com.github.jntakpe.commons.mongo.SbReactiveMongoRepository
 import com.github.jntakpe.commons.test.TestDataProvider
+import com.github.jntakpe.commons.test.TestInitializer
 import reactor.core.publisher.Mono
 
 abstract class MongoDao<T : Identifiable>(
     private val repository: SbReactiveMongoRepository<T>,
     private val dataProvider: TestDataProvider<T>,
-) {
+) : TestInitializer<T> {
 
-    fun init() = dataProvider.data().toList().also { clean().thenMany(repository.saveAll(it)).blockLast() }
+    override fun init() = dataProvider.data().toList().also { clean().thenMany(repository.saveAll(it)).blockLast() }
 
-    fun clean(): Mono<Void> = repository.deleteAll()
+    override fun clean(): Mono<Void> = repository.deleteAll()
 
     fun count() = repository.count().block() ?: 0
 }
