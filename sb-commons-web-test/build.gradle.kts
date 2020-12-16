1fd8186a65080155700a59c203179969ed0b0e94
jntakpe <jntakpe@gmail.com>
2020-12-16 22:46:11 +0000

    build: init commons-web-test module

---
+++ sb-commons-web-test/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("plugin.spring") version "1.4.21"
+}
+
+dependencies {
+    api(project(":sb-commons-test"))
+    api(project(":sb-commons-web"))
+    api("org.springframework.cloud:spring-cloud-starter-contract-verifier")
+    api("io.rest-assured:spring-web-test-client")
+}
